623f924546b89c69b1987a6308f459b3ff04e813
sinhaZ <sinhaafroz16@gmail.com>
2022-12-27 17:24:24 +0600

    input field and css file added

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE-edge" />
+    <meta name="viewport" content="width=device-width" , initial-scale="1.0" />
+    <title>Assesment</title>
+    <link rel="stylesheet" href="style.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css"
+      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+      crossorigin="anonymous"
+    />
+  </head>
+  <body>
+    <div class="container">
+      <div class="main">
+        <div class="clientForm">
+          <h1>Enter Enquiries</h1>
+          <form method="post">
+            <div class="mb-3">
+              <label for="clientName" class="form-label">Full Name</label>
+              <input
+                type="name"
+                class="form-control"
+                id="clientName"
+                name="name"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
